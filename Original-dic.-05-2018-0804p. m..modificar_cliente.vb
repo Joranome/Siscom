2aab77d95f79e9296dca8e93c437114aa21237d9
Raúl Noriega <43706625+Joranome@users.noreply.github.com>
2019-12-12 12:01:19 -0700

    Initial commit

---
+++ Original-dic.-05-2018-0804p. m..modificar_cliente.vb
@@ -0,0 +1,97 @@
+﻿Public Class modificar_cliente
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        If txtCodCliente.Text <> "" Or txtNomCliente.Text <> "" Then
+            txtCliente.Enabled = True
+            txtDireccion.Enabled = True
+            txtTel.Enabled = True
+            txtCodCliente.Enabled = False
+            txtNomCliente.Enabled = False
+            btnBuscar.Enabled = False
+            txtCliente.Select()
+        Else
+            txtCliente.Enabled = False
+            txtDireccion.Enabled = False
+            txtTel.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtCodCliente_TextChanged(sender As Object, e As EventArgs) Handles txtCodCliente.TextChanged, txtNomCliente.TextChanged
+        If txtCodCliente.Text <> "" Or txtNomCliente.Text <> "" Then
+            btnBuscar.Enabled = True
+        Else
+            btnBuscar.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtCliente_TextChanged(sender As Object, e As EventArgs) Handles txtCliente.TextChanged, txtTel.TextChanged, txtDireccion.TextChanged
+        If txtCliente.Text <> "" And txtDireccion.Text <> "" And (txtTel.Text.Replace(" ", "").Count() = 12) Then
+            btnModificar.Enabled = True
+        Else
+            btnModificar.Enabled = False
+        End If
+    End Sub
+
+    Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
+        If (txtCliente.Text = "" And txtDireccion.Text = "" And Not (txtTel.Text.Replace(" ", "").Count() = 12)) Then
+            cliente.Show()
+            Me.Close()
+        Else
+            If (MessageBox.Show("¿Seguro que desea salir?", "Salir", MessageBoxButtons.YesNo) = DialogResult.Yes) Then
+                cliente.Show()
+                Me.Close()
+
+            End If
+        End If
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        MessageBox.Show("Se ha modificado el cliente")
+        txtCliente.Text = ""
+        txtDireccion.Text = ""
+        txtTel.Text = ""
+        txtCliente.Enabled = False
+        txtDireccion.Enabled = False
+        txtTel.Enabled = False
+        txtCodCliente.Text = ""
+        txtNomCliente.Text = ""
+        btnBuscar.Enabled = True
+        txtCodCliente.Enabled = True
+        txtNomCliente.Enabled = True
+        btnModificar.Enabled = False
+        txtCodCliente.Select()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub txtTel_MaskInputRejected(sender As Object, e As MaskInputRejectedEventArgs) Handles txtTel.MaskInputRejected
+        If (txtTel.Text.Replace(" ", "").Count() >= 7) Then
+            If txtDireccion.Text <> "" And txtCliente.Text <> "" Then
+                btnModificar.Enabled = True
+            Else
+                btnModificar.Enabled = False
+            End If
+        Else
+            btnModificar.Enabled = False
+        End If
+    End Sub
+
+    Private Sub txtCliente_KeyDown(sender As Object, e As KeyEventArgs) Handles txtCliente.KeyDown, txtTel.KeyDown, txtDireccion.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If (txtTel.Text.Replace(" ", "").Count() >= 7) Then
+                If txtDireccion.Text <> "" And txtCliente.Text <> "" Then
+                    btnModificar_Click(sender, e)
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub txtCodCliente_KeyDown(sender As Object, e As KeyEventArgs) Handles txtCodCliente.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            If txtNomCliente.Text <> "" Or txtCodCliente.Text <> "" Then
+                btnBuscar_Click(sender, e)
+            End If
+        End If
+    End Sub
+End Class
---
+++ ~AutoRecover.Inventario.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Inventario
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Inventario))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lbl_credito = New System.Windows.Forms.Label()
+        Me.lblEmpresa = New System.Windows.Forms.Label()
+        Me.imgEmpresa = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.imgEmpresa, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.lbl_credito)
+        Me.Panel1.Location = New System.Drawing.Point(149, 115)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(470, 452)
+        Me.Panel1.TabIndex = 13
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Image = CType(resources.GetObject("Button5.Image"), System.Drawing.Image)
+        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button5.Location = New System.Drawing.Point(77, 224)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(120, 87)
+        Me.Button5.TabIndex = 19
+        Me.Button5.Text = "Modificar"
+        Me.Button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
+        Me.Button4.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button4.Location = New System.Drawing.Point(168, 356)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(120, 67)
+        Me.Button4.TabIndex = 18
+        Me.Button4.Text = "Volver"
+        Me.Button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
+        Me.Button3.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button3.Location = New System.Drawing.Point(262, 224)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(120, 87)
+        Me.Button3.TabIndex = 17
+        Me.Button3.Text = "Eliminar"
+        Me.Button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button2.Location = New System.Drawing.Point(262, 89)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 87)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Buscar"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LimeGreen
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button1.Location = New System.Drawing.Point(77, 89)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 87)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Agregar"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'lbl_credito
+        '
+        Me.lbl_credito.AutoSize = True
+        Me.lbl_credito.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_credito.Location = New System.Drawing.Point(162, 27)
+        Me.lbl_credito.Name = "lbl_credito"
+        Me.lbl_credito.Size = New System.Drawing.Size(134, 31)
+        Me.lbl_credito.TabIndex = 0
+        Me.lbl_credito.Text = "Inventario"
+        '
+        'lblEmpresa
+        '
+        Me.lblEmpresa.AutoSize = True
+        Me.lblEmpresa.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEmpresa.Location = New System.Drawing.Point(173, 53)
+        Me.lblEmpresa.Name = "lblEmpresa"
+        Me.lblEmpresa.Size = New System.Drawing.Size(242, 46)
+        Me.lblEmpresa.TabIndex = 12
+        Me.lblEmpresa.Text = "Modelorama"
+        '
+        'imgEmpresa
+        '
+        Me.imgEmpresa.Image = CType(resources.GetObject("imgEmpresa.Image"), System.Drawing.Image)
+        Me.imgEmpresa.Location = New System.Drawing.Point(24, 41)
+        Me.imgEmpresa.Name = "imgEmpresa"
+        Me.imgEmpresa.Size = New System.Drawing.Size(106, 104)
+        Me.imgEmpresa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.imgEmpresa.TabIndex = 11
+        Me.imgEmpresa.TabStop = False
+        '
+        'Inventario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 590)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblEmpresa)
+        Me.Controls.Add(Me.imgEmpresa)
+        Me.Name = "Inventario"
+        Me.Text = "Inventario"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.imgEmpresa, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lbl_credito As Label
+    Friend WithEvents lblEmpresa As Label
+    Friend WithEvents imgEmpresa As PictureBox
+End Class
